0103e03155d36c7c395a94116efe2869f6b814b2
AnnieShen <s1002732@gmail.com>
2018-09-08 12:17:33 +0800

    first-commit

--- homeworks/week1/hw5.js
+++ homeworks/week1/hw5.js
@@ -1,7 +1,18 @@
-function join(str, concatStr) {
+function join(str, concatStr) { 
 
+  var result = str.join(concatStr);
+  return result;
+  console.log(result);
+  
 }
 
-function repeat(str, times) {
 
+function repeat(str, times) { 
+  
+  var array = []; 
+  for (i = 0; i < times; i++) { 
+    array.push(str); 
+  } 
+  return array.join('');
+  console.log(repeat(str, times));
 }
