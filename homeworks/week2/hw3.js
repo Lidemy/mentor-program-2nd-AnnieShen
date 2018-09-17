33539a03aeabc600992be24fbeeb3f8fb56b0808
AnnieShen <s1002732@gmail.com>
2018-09-17 22:44:30 +0800

    week2-fixed-1

--- homeworks/week2/hw3.js
+++ homeworks/week2/hw3.js
@@ -1,5 +1,5 @@
 function isPrime(n) {
-    for(i = 2; i <= n-1; i++){
+    for(var i = 2; i <= n-1; i++){
       if(n % i == 0){
         return false
       }
--- homeworks/week2/hw5.js
+++ homeworks/week2/hw5.js
@@ -13,14 +13,16 @@ function add(a, b) {
     for(var i = 1; i <= abLength; i++){
       bArray.unshift('0')
       //console.log(aArray)
+      //console.log(bArray)
   
     }
   
-  }else{
+  }else if(bArray.length > aArray.length){
     var baLength = bArray.length - aArray.length
-    for(var i = 1; i <= baLength; i++){
+    for(var m = 1; m <= baLength; m++){
       aArray.unshift('0')
-      console.log(bArray)
+      //console.log(aArray)
+      //console.log(bArray)
     }
   }
   
@@ -30,13 +32,19 @@ function add(a, b) {
   }
   
   
-  //console.log(sumArray)
+  console.log(sumArray)
   
   for(var k = 0; k < sumArray.length; k++){
     if(sumArray[k] >= 10){
       sumArray[k] = sumArray[k] - 10;
       //sumArray.push(0)
       sumArray[k + 1] += 1;
+      //console.log("要進位"+sumArray)
+      
+      if(sumArray[sumArray.length-1] >=10 ){
+        sumArray.push(0)
+      }
+      
     }
   
   }
