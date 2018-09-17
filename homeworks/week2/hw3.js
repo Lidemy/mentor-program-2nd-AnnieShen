7add7cbc0b349eb48e810b468f3a05d777ae46ca
AnnieShen <s1002732@gmail.com>
2018-09-17 23:07:57 +0800

    fixed-hw3

--- homeworks/week2/hw3.js
+++ homeworks/week2/hw3.js
@@ -1,10 +1,15 @@
 function isPrime(n) {
-    for(var i = 2; i <= n-1; i++){
+  if(n===1){
+    return false
+  }else{
+  
+  for(var i = 2; i <= n-1; i++){
       if(n % i == 0){
         return false
       }
   　}
      return true
   }
+}
 
 module.exports = isPrime
