6aa1200b6115314e0e07621812d44ac819a886af
AnnieShen <s1002732@gmail.com>
2018-11-05 21:43:33 +0800

    fixed-hw2

--- homeworks/week8/hw2/deleteComments.php
+++ homeworks/week8/hw2/deleteComments.php
@@ -3,13 +3,8 @@
   require_once('conn.php');
   
   //判斷是否登入過
-  if(isset($_SESSION["member_id"])) {
-    $stmt = $conn->prepare("SELECT * FROM annieshen_users WHERE user_id=?");
-    $stmt->bind_param("s", $_SESSION["member_id"]);
-    $stmt->execute();
-    $result = $stmt->get_result();
-    $row = $result->fetch_assoc();
-    $user_id=$row['user_id'];
+   if(isset($_SESSION["member_id"])) {
+    $user_id=$_SESSION["member_id"];
   } 
 
   // 刪除主留言
@@ -24,12 +19,13 @@
       $arr = array('result' => 'success');
       echo json_encode($arr);
     }else{
-      echo json_encode('{"success": fail}');
+      $arr = array('result' => 'fail');
+      echo json_encode($arr);
     }
     
 }
 
 
-  $stmt->close();
+  $stmt_deleteSql->close();
 
 ?>
